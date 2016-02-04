a848749db86c57f73fc0cbdca8a2b002b6aa2739
Ronan Connolly <hi@ronanconnolly.ie>
2016-02-04 18:34:45 +0000

    Encapsulated database logic into a service Now using local and remote db Using promises to wait 100ms before refreshing view with list from service (after remove, add or edit)

--- app/scripts/controllers/auctions.js
+++ app/scripts/controllers/auctions.js
@@ -12,169 +12,52 @@
     angular.module('allsop')
         .controller('AuctionsCtrl', AuctionsCtrl);
 
-    // AuctionsCtrl
-    // .$inject = [];
-
-    function AuctionsCtrl($scope, $q, $timeout, $rootScope) {
+    function AuctionsCtrl($q, $scope, auctionService) {
         // vm for viewmodel
         var vm = this;
 
-        // vm.auctionTitles = [];
         vm.auctionEntries = [];
-
-        // pouch
-        var db;
-        var remote;
-
         vm.init = init;
+        vm.refreshList = refreshList;
         vm.removeEntry = removeEntry;
         vm.addEntry = addEntry;
-        init();
+        // vm.editEntry = editEntry;
         
-        // add entry
-        function addEntry(entry) {
-            entry.when = entry.date.substring(0, 15) + ' ' + entry.time;
-            delete entry.date;
-            delete entry.time;
+        init();
 
-            entry._id = formatISO(entry.when);
-
-            db.put(entry);
-
-            console.log('new entry: ' + JSON.stringify(entry));
-        };
-
-
+        function refreshList() {
+            // auctionService.getEntries();
+            vm.auctionEntries = auctionService.auctionEntries;
+        }
 
         function init() {
-            // vm.auctionTitles = [];
-            vm.auctionEntries = [];
+            auctionService.init();
+            vm.regex = '\\d{2}:\\d{2}';
+            // vm.regex = '\d{2}:\d{2}';
+            auctionService.getEntries()
+            vm.refreshList();
+        }
 
-            // not using a local db as not needed in browser (also the replication caused the changes method to fire way too many times)
-            // db = new PouchDB('auctions');
-            // db = new PouchDB('http://localhost:5984/auctions');
-            db = new PouchDB('https://desimeentsteryingespelte:428b3ecba6fcddf3536a9776726431b6a3a89d40@ianmcloughlin.cloudant.com/auctions');
+        function addEntry(entry) {
+            var def = $q.defer();
+            auctionService.addEntry(entry, def);
             
-            vm.regex = '\d{2}:\d{2}';
-            pouchInit();
-        }
-
-        function formatISO(isostr) {
-
-            var dateToConvert = isostr + ':00 GMT+0000 (GMT)';
-            var newDate = new Date(dateToConvert);
-            var dateNum = Number(newDate);
-            var dateStr = dateNum.toString();
-
-            // console.log('time stamp: ' + isostr);
-            // console.log('human readable: ' + parsedDate);
-
-            return dateStr;
-        }
-        
-        // remove entry
-        function removeEntry(entry, index) {
-            vm.auctionEntries.splice(index, 1);
-            db.remove(entry);
-
-            // $timeout(function () { $rootScope.$apply(); });
+            // waits for entry to be added to the remote db
+            def.promise.then(function () {
+                // console.log("ready");
+                vm.refreshList();
+            });
         };
 
-        
-        // cannot 2 way bind to a service
-        // need to have pouch here for real time binding
-        function pouchInit() {
-            // var firstSync = false;
+        function removeEntry(entry) {
+            var def = $q.defer();
+            auctionService.removeEntry(entry, def);
             
-
-            var opts = { live: true };
-            // db.sync(db, remote);
-            // db.replicate.to(remote, opts); // third arg for function error
-            // db.replicate.from(remote, opts);
-
-            // get data
-            showEntries();
-            vm.showEntries = showEntries;
-       
-            // updates view when db changes
-            db.changes({
-                since: 'now',
-                live: true
-            }).on('change', function () {
-                vm.showEntries();
+            // waits for entry to be added to the remote db
+            def.promise.then(function () {
+                // console.log("ready");
+                vm.refreshList();
             });
-
-            // vm.refreshList = refreshList;
-
-            // function refreshList() {
-            //     $q.when(showEntries())
-            //         .then(function () {
-            //             // vm.auctionTitles = [];
-            //             vm.auctionEntries = [];
-            //             db.allDocs({ include_docs: true, descending: true }, function (err, doc) {
-            //                 console.log('DB Change, updating list...');
-            //                 // console.log('doc: ' + JSON.stringify(doc.rows));
-            //                 addListEntry(doc.rows);
-            //             });
-
-            //             $timeout(function () { $rootScope.$apply(); });
-            //         });
-            // }
-            
-            vm.showEntries = showEntries;
-            
-            // show entries
-            function showEntries() {
-                // vm.auctionTitles = [];
-                vm.auctionEntries = [];
-                db.allDocs({ include_docs: true, descending: true }, function (err, doc) {
-                    console.log('DB Change, updating list...');
-                    // console.log('doc: ' + JSON.stringify(doc.rows));
-                    addListEntry(doc.rows);
-                });
-                
-                $timeout(function () { $rootScope.$apply(); });
-                
-                return;
-            }
-
-            function addListEntry(entries) {
-                entries.forEach(function (entry) {
-                    if (entry.id.charAt(0) !== '_') {
-                        // vm.auctionTitles.push(entry.doc.where);
-
-                        entry.doc.when = formatDate(entry.doc._id);
-                        vm.auctionEntries.push(entry.doc);
-                        
-                        // vm.auctionObjects.push({
-                        //     _id: entry.doc._id,
-                        //     _rev: 
-                        // });
-                    }
-                });
-
-                // console.log(vm.auctionTitles);
-                console.log(vm.auctionEntries);
-                
-                
-                // This just makes sure that the template is refreshed once the list is updated.
-                // Sometimes PouchDB changes fall through the cracks otherwise.
-                // IMPORTANT
-                $timeout(function () { $rootScope.$apply(); });
-            }
-
-            function formatDate(timestr) {
-                var asDate = new Date(parseInt(timestr));
-                var parsedDate = asDate.toString().slice(0, 21);
-
-                // console.log("time stamp: " + timestr);
-                // console.log("human readable: " + parsedDate);
-
-                return parsedDate;
-            }
-
-            vm.showEntries();
-
-        }
+        };
     }
 })();
---
+++ app/scripts/services/auctionservice.js
@@ -0,0 +1,159 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name allsop.auctionService
+ * @description
+ * # auctionService
+ * Service in the allsop.
+ */
+angular.module('allsop')
+    .service('auctionService', function ($timeout, $rootScope, $q) {
+        // AngularJS will instantiate a singleton by calling "new" on this function
+        var vm = this;
+        var local = new PouchDB('auctions');
+        var remote = new PouchDB('https://desimeentsteryingespelte:428b3ecba6fcddf3536a9776726431b6a3a89d40@ianmcloughlin.cloudant.com/auctions');
+        var db = local;
+
+        vm.auctionEntries = [];
+        vm.init = init;
+        vm.getEntries = getEntries;
+        vm.addEntry = addEntry;
+        vm.removeEntry = removeEntry;
+        vm.editEntry = editEntry;
+
+        function init() {
+            // console.log("init");
+
+            db.changes({
+                since: 'now',
+                live: true
+            }).on('change', function () {
+                vm.getEntries();
+            });
+
+            local.sync(remote, {
+                live: true
+            }).on('change', function (change) {
+                // yo, something changed!
+            }).on('error', function (err) {
+                // yo, we got an error! (maybe the user went offline?)
+            });
+
+
+        }
+
+        function getEntries() {
+            vm.auctionEntries = [];
+            db.allDocs({ include_docs: true, descending: true }, function (err, doc) {
+                console.log('DB Change, updating list...');
+                // console.log('doc: ' + JSON.stringify(doc.rows));
+                addListEntry(doc.rows);
+            });
+
+            $timeout(function () { $rootScope.$apply(); });
+
+            return vm.auctionEntries;
+        }
+
+        // private
+        function addListEntry(entries) {
+            entries.forEach(function (entry) {
+                if (entry.id.charAt(0) !== '_') {
+                    // vm.auctionTitles.push(entry.doc.where);
+
+                    entry.doc.when = formatDate(entry.doc._id);
+                    vm.auctionEntries.push(entry.doc);
+                        
+                    // vm.auctionObjects.push({
+                    //     _id: entry.doc._id,
+                    //     _rev: 
+                    // });
+                }
+            });
+
+            // console.log(vm.auctionTitles);
+            // console.log(vm.auctionEntries);
+                
+                
+            // This makes sure that the template is refreshed once the list is updated.
+            // Sometimes PouchDB changes fall through the cracks otherwise.
+            // IMPORTANT
+            $timeout(function () { $rootScope.$apply(); });
+        }
+        
+        // private
+        function formatDate(timestr) {
+            var asDate = new Date(parseInt(timestr));
+            var parsedDate = asDate.toString().slice(0, 21);
+
+            // console.log("time stamp: " + timestr);
+            // console.log("human readable: " + parsedDate);
+
+            return parsedDate;
+        }
+
+        function addEntry(entry, def) {
+            // console.log("add");
+
+            entry.when = entry.date.substring(0, 15) + ' ' + entry.time;
+            delete entry.date;
+            delete entry.time;
+
+            entry._id = formatISO(entry.when);
+
+            // db.put(entry);
+
+
+            db.put(entry).then(function (response) {
+                // waits for the onchange event to update the local list from the remote db
+                $timeout(function () {
+                    def.resolve();
+                    // console.log('update with timeout fired')
+                }, 100);
+
+            }).catch(function (err) {
+                console.log(err);
+            });
+            
+            
+            // vm.auctionEntries.push(entry);
+            console.log("vm.auctionEntries: " + JSON.stringify(vm.auctionEntries));
+            console.log('new entry: ' + JSON.stringify(entry));
+
+            return def;
+        }
+        
+        // private
+        function formatISO(isostr) {
+            var dateToConvert = isostr + ':00 GMT+0000 (GMT)';
+            var newDate = new Date(dateToConvert);
+            var dateNum = Number(newDate);
+            var dateStr = dateNum.toString();
+
+            // console.log('time stamp: ' + isostr);
+            // console.log('human readable: ' + parsedDate);
+
+            return dateStr;
+        }
+
+        function removeEntry(entry, def) {
+            // console.log("remove");
+            db.remove(entry).then(function () {
+                // waits for the onchange event to update the local list from the remote db
+                $timeout(function () {
+                    def.resolve();
+                    // console.log('update with timeout fired')
+                }, 100);
+            });
+            // vm.auctionEntries.splice(index, 1);
+            
+            return def;
+        }
+
+        function editEntry(entry) {
+            console.log("edit");
+        }
+
+        return vm;
+    });
