79799d0d82767f2b42f9fcdc04e823aecbd68383
Ronan Connolly <hi@ronanconnolly.ie>
2016-02-07 03:02:58 +0000

    update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
-// app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist/'));
 
 app.get('/', function (req, res) {
     console.log('welcome to route /');
